a88a13930ce64e30a8b343cdfc960ee657a93acf
Bendzsike <bodor.benjamin@outlook.com>
2021-04-28 01:46:48 +0300

    Changed up measure method to System.currentTimeMillis(). Remeasured everything, and rewrote the measurements in OOP_LAB9.pdf. Also measured the update10000000.txt as well, and wrote the results in the pdf!

--- Labor_9/src/labor9_3/Main.java
+++ Labor_9/src/labor9_3/Main.java
@@ -13,6 +13,5 @@ public class Main {
         System.out.println("Amount of items updated: " + amount);
         System.out.println(end - start);
 //        storage.print();
-//        System.out.println("asd");
     }
 }
